7705d34c9fcba42806fe27825e02cb308132fc65
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2014-04-18 01:44:24 -0300

    Build fixed, added missing files. Documentation file renamed.

---
+++ C#/GameEngine.Sdk/GameMoveReceivedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GameEngine.Sdk.Interfaces;
+
+namespace GameEngine.Sdk
+{
+	public class GameMoveReceivedEventArgs : EventArgs
+	{
+		public IGameMove<T> GetMove<T> ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
