cfa1c3861169a13c11e7e6ec78339fb234ee5e8c
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2014-04-28 01:50:11 -0300

    Server project overrides implemented, Contracts implemented and packages added

--- C#/GuessMyNumber.Core/Game/GuessMyNumberPlayer.cs
+++ C#/GuessMyNumber.Core/Game/GuessMyNumberPlayer.cs
@@ -7,11 +7,12 @@ namespace GuessMyNumber.Core.Game
     public class GuessMyNumberPlayer : IGamePlayer<INumber, IAttemptResult>
     {
         private readonly INumberComparer numberComparer;
-        private INumber playerNumber;
 
         public bool IsPlaying { get; set; }
 
         public string Name { get; set; }
+
+        public INumber Number { get; private set; }
 
         public GuessMyNumberPlayer()
         {
@@ -20,12 +21,19 @@ namespace GuessMyNumber.Core.Game
 
         public void AssignNumber(INumber number)
         {
-            this.playerNumber = number;
+            if (this.Number != default(INumber))
+            {
+                var errorMessage = string.Format("The number of Player {0} can't be changed once assigned", this.Name);
+
+                throw new ApplicationException(errorMessage);
+            }
+
+            this.Number = number;
         }
 
         public IGameMoveResponse<IAttemptResult> ProcessMove(IGameMove<INumber> move)
         {
-            if (this.playerNumber == default(INumber))
+            if (this.Number == default(INumber))
             {
                 var errorMessage = string.Format("The player {0} is not ready because it doesn't have a number assigned", this.Name);
 
@@ -33,7 +41,7 @@ namespace GuessMyNumber.Core.Game
             }
 
             var triedNumber = move.MoveObject;
-            var result = this.numberComparer.Compare(this.playerNumber, triedNumber);
+            var result = this.numberComparer.Compare(this.Number, triedNumber);
 
             return new GuessMyNumberResponse(result);
         }
---
+++ C#/GuessMyNumber.Server/Contracts/GuessMyNumberMoveRequestObject.cs
@@ -0,0 +1,13 @@
+﻿using Gamify.Server.Contracts.Requests;
+
+namespace GuessMyNumber.Server.Contracts
+{
+    public class GuessMyNumberMoveRequestObject : IRequestObject
+    {
+        public string SessionId { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public string Number { get; set; }
+    }
+}
